e83e89ee8f464a9af0e410890e6441d0b0796c31
viclee25 <viclee25@126.com>
2017-11-27 19:52:45 +0800

    1.0

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styles: []
+})
+export class MessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
